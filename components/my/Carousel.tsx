0fe5c7620f56f9ae780641e9ee1b8c9c168e0a06
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-19 10:08:55 +0100

    added. base structure to Carousel

--- components/my/Carousel.tsx
+++ components/my/Carousel.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import styles from "@/styles/styles";
+import { Image, Text, View } from "react-native";
 
 export default function Carousel() {
   return (
-    <View>
-      <Text>Sono Carousel</Text>
+    <View style={styles.containerCarousel}>
+      <View style={styles.containerCarouselPages}>
+        <View style={styles.containerCarouselPage}>
+          <Text style={styles.titleItem}>Pagina 1</Text>
+          <Image style={styles.titleImage}></Image>
+        </View>
+        <View style={styles.containerCarouselPage}>
+          <Text style={styles.titleItem}>Pagina 2</Text>
+          <Image style={styles.titleImage}></Image>
+        </View>
+      </View>
+
+      <View style={styles.containerCarouselPages}>
+        <View style={styles.containerCarouselPage}>
+          <Text style={styles.titleItem}>Pagina 1</Text>
+          <Image style={styles.titleImage}></Image>
+        </View>
+        <View style={styles.containerCarouselPage}>
+          <Text style={styles.titleItem}>Pagina 2</Text>
+          <Image style={styles.titleImage}></Image>
+        </View>
+      </View>
     </View>
   );
 }
